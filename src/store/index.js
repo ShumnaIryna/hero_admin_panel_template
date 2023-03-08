3fef315fad2b260bc467077c8eafbad88d13eec2
ShumnaIryna <87974828+ShumnaIryna@users.noreply.github.com>
2023-03-08 13:14:01 +0200

    adedd Middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,17 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
+
+//------LESSON-202
+const stringMiddleware = (createStore) => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
+    }
+    return next(action)
+};
+//------LESSON-202-THE END
 
 const enhancer = (createStore) => (...args) => {
     const store = createStore(...args);
@@ -16,12 +27,19 @@ const enhancer = (createStore) => (...args) => {
     }
     return store;
 }
-const store = createStore( 
-                        combineReducers({heroes, filters}), 
-                        compose(
-                            enhancer,
-                            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                        ));
+// const store = createStore( 
+//                         combineReducers({heroes, filters}), 
+//                         compose(
+//                             enhancer,
+//                             window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//                         ));
 
+//------LESSON-202
+const store = createStore( 
+    combineReducers({heroes, filters}), 
+    compose(applyMiddleware(stringMiddleware)),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+//------LESSON-202-THE END
 export default store;
 
