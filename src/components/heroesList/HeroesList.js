5514be022fd5fcba45c1e458702ad73cfec7eb3d
ShumnaIryna <87974828+ShumnaIryna@users.noreply.github.com>
2023-03-14 14:23:39 +0200

    applied the function createAsyncThunk

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -2,10 +2,10 @@ import {useHttp} from '../../hooks/http.hook';
 import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup} from 'react-transition-group';
-import { createSelector } from 'reselect';
+import { createSelector } from '@reduxjs/toolkit';
 
-import { fetchHeroes } from '../../actions';
-import {heroDeleted} from './heroesSlice';
+//import { fetchHeroes } from '../../actions';
+import {heroDeleted, fetchHeroes} from './heroesSlice';
 
 
 import HeroesListItem from "../heroesListItem/HeroesListItem";
@@ -47,7 +47,8 @@ const HeroesList = () => {
 
     useEffect(() => {
         //dispatch(heroesFetching());
-        dispatch(fetchHeroes(request));
+        //dispatch(fetchHeroes(request));
+        dispatch(fetchHeroes());
 
         // eslint-disable-next-line
     }, []);
